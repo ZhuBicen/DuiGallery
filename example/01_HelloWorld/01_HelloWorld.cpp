71dfe1d06ccf3b13112402da58eecdc4c26014d4
bzhu <zhu.bicen@gmail.com>
2014-08-28 23:56:21 +0800

    Update HelloWord example, to show the base ControlUI elements.

--- example/01_HelloWorld/01_HelloWorld.cpp
+++ example/01_HelloWorld/01_HelloWorld.cpp
@@ -12,12 +12,33 @@ public:
 
 		if (uMsg == WM_CREATE)
 		{
-			CControlUI *pWnd = new CButtonUI;
-			pWnd->SetText(_T("Hello World"));   // 设置文字
-			pWnd->SetBkColor(0xFF00FF00);       // 设置背景色
+            CVerticalLayoutUI* layout = new CVerticalLayoutUI;
+            layout->SetBkColor(0xFFFFFFFF);
 
+            {
+                auto button = new CButtonUI;
+                button->SetBorderSize(8);
+                button->SetBorderColor(0xFF1122cc);
+                button->SetPadding(RECT{ 10, 10, 10, 0 });
+                button->SetFixedHeight(40);
+                button->SetText(_T("Hello Button1!"));
+                button->SetFocusBorderColor(0x11FF33);
+                button->SetBorderRound(SIZE{ 5, 5 });
+                layout->Add(button);
+            }
+            {
+                auto button = new CLabelUI;
+                button->SetBorderSize(2);
+                button->SetBorderColor(0xFF1122cc);
+                button->SetFixedHeight(40);
+                button->SetPadding(RECT{ 10, 10, 10, 10 });
+                button->SetText(_T("    Hello Label!"));
+                button->SetBkColor(0xFF33FFF0);
+                layout->Add(button);
+            }
 			m_PaintManager.Init(m_hWnd);
-			m_PaintManager.AttachDialog(pWnd);
+			m_PaintManager.AttachDialog(layout);
+            // m_PaintManager.SetShowUpdateRect(true);
 			return lRes;
 		}
 		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
@@ -27,8 +48,7 @@ public:
 
 		return __super::HandleMessage(uMsg, wParam, lParam);
 	}
-
-protected:
+private:
 	CPaintManagerUI m_PaintManager;
 };
 
@@ -37,7 +57,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
 	CPaintManagerUI::SetInstance(hInstance);
 
 	CDuiFrameWnd duiFrame;
-	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.Create(NULL, _T("Hello DuiGallery"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
 	duiFrame.ShowModal();
 	return 0;
 }
