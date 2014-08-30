e0138d3e1b9c9bda51d7a615ed811e48f17ac6b2
bzhu <zhu.bicen@gmail.com>
2014-08-30 09:30:57 +0800

    Show button usage

--- example/01_HelloWorld/01_HelloWorld.cpp
+++ example/01_HelloWorld/01_HelloWorld.cpp
@@ -13,27 +13,62 @@ public:
 		if (uMsg == WM_CREATE)
 		{
             CVerticalLayoutUI* layout = new CVerticalLayoutUI;
-            layout->SetBkColor(0xFFFFFFFF);
-
+            layout->SetBkColor (0xFFFFFFFF);
+            layout->SetChildPadding(10);
+            layout->SetInset(RECT{ 10, 10, 10, 10 });
             {
                 auto button = new CButtonUI;
-                button->SetBorderSize(8);
-                button->SetBorderColor(0xFF1122cc);
-                button->SetPadding(RECT{ 10, 10, 10, 0 });
+                button->SetBkColor(0xFFFF0000);
                 button->SetFixedHeight(40);
-                button->SetText(_T("Hello Button1!"));
-                button->SetFocusBorderColor(0x11FF33);
-                button->SetBorderRound(SIZE{ 5, 5 });
                 layout->Add(button);
             }
             {
-                auto button = new CLabelUI;
-                button->SetBorderSize(2);
-                button->SetBorderColor(0xFF1122cc);
+                auto button = new CButtonUI;
                 button->SetFixedHeight(40);
+                button->SetBkColor(0xFFFF0000);
+                button->SetBkColor2(0xFF00FF00);
+                layout->Add(button);
+            }
+            {
+                auto button = new CButtonUI;
+                button->SetFixedHeight(40);
+                button->SetBkColor(0xFFFF0000);
+                button->SetBkColor2(0xFF00FF00);
+                button->SetBkColor3(0xFF0000FF);
+                layout->Add(button);
+            }
+            {
+                auto button = new CButtonUI;
+                button->SetBorderSize(2);
+                button->SetBorderColor(0xFFFF0000);
+                button->SetFixedHeight(40);
+                button->SetBkColor(0xFFD6DBE9);
+                layout->Add(button);
+            }
+            {
+                auto button = new CButtonUI;
+                button->SetBorderSize(5);
+                button->SetBorderRound(SIZE{ 5, 5 });
+                button->SetBorderColor(0xFF00FF00);
+                button->SetFocusBorderColor(0xFFFF0000);
+                button->SetBkColor(0xFFD6DBE9);
+                button->SetFixedHeight(40);
+                layout->Add(button);
+            }
+            {
+                auto button = new CButtonUI;
+                button->SetBkColor(0xFFD6DBE9);
+                button->SetLeftBorderSize(1);
+                button->SetTopBorderSize(2);
+                button->SetRightBorderSize(3);
+                button->SetBottomBorderSize(24);
+                button->SetBorderStyle(2);
+                button->SetBorderColor(0xFFFF0000);
+                button->SetFocusBorderColor(0xFF00FF00);
+                button->SetFixedHeight(40);
+                // button->SetFixedWidth(200);
                 button->SetPadding(RECT{ 10, 10, 10, 10 });
-                button->SetText(_T("    Hello Label!"));
-                button->SetBkColor(0xFF33FFF0);
+                button->SetText(_T("Button"));
                 layout->Add(button);
             }
 			m_PaintManager.Init(m_hWnd);
@@ -57,7 +92,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
 	CPaintManagerUI::SetInstance(hInstance);
 
 	CDuiFrameWnd duiFrame;
-	duiFrame.Create(NULL, _T("Hello DuiGallery"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.Create(NULL, _T("Hello DuiGallery"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE, 0, 0, 500, 800);
 	duiFrame.ShowModal();
 	return 0;
 }
